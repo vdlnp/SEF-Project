4800cd99c627398979820b1392305c504c45fed2
Viandra Lika Nur Prastya <viandralika@yahoo.com>
2026-02-02 17:41:28 +0800

    db updated

--- sef_project.sql
+++ sef_project.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 01, 2026 at 06:58 PM
+-- Generation Time: Feb 02, 2026 at 10:40 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,6 +45,77 @@ INSERT INTO `project` (`id`, `title`, `description`, `deadline`, `room_code`) VA
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `proposal`
+--
+
+CREATE TABLE `proposal` (
+  `id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `lead_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `deadline` date DEFAULT NULL,
+  `status` enum('under_review','reviewed','approved','rejected') DEFAULT 'under_review',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `proposal`
+--
+
+INSERT INTO `proposal` (`id`, `project_id`, `lead_id`, `title`, `description`, `deadline`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, 2, 'Proposal Draft', 'This is a draft', '2026-02-19', '', '2026-02-02 08:39:51', '2026-02-02 08:39:51'),
+(2, 1, 2, 'Proposal Draft 2', 'This is ANOTHER proposal', NULL, '', '2026-02-02 08:59:29', '2026-02-02 08:59:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proposal_attachments`
+--
+
+CREATE TABLE `proposal_attachments` (
+  `id` int(11) NOT NULL,
+  `proposal_id` int(11) NOT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `proposal_attachments`
+--
+
+INSERT INTO `proposal_attachments` (`id`, `proposal_id`, `file_name`, `file_path`, `uploaded_at`) VALUES
+(1, 1, 'proposal.txt', 'uploads/proposals/1770021591_proposal.txt', '2026-02-02 08:39:51'),
+(2, 2, 'proposal.txt', 'uploads/proposals/1770022769_proposal.txt', '2026-02-02 08:59:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proposal_reviews`
+--
+
+CREATE TABLE `proposal_reviews` (
+  `id` int(11) NOT NULL,
+  `proposal_id` int(11) DEFAULT NULL,
+  `reviewer_id` int(11) NOT NULL,
+  `comments` text DEFAULT NULL,
+  `score` decimal(3,1) DEFAULT NULL,
+  `status` varchar(20) DEFAULT 'pending',
+  `reviewed_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `proposal_reviews`
+--
+
+INSERT INTO `proposal_reviews` (`id`, `proposal_id`, `reviewer_id`, `comments`, `score`, `status`, `reviewed_at`) VALUES
+(1, 1, 1, 'This is fantastic', 9.0, 'reviewed', '2026-02-02 17:22:14');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -65,7 +136,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `room_code`, `status`) VALUES
 (1, 'Content Coordinator', 'user@admin.com', 'admin123', 'Admin', NULL, 'active'),
 (2, 'Ali  bin Abu', 'Ali@gmail.com', 'User@12345', 'Project Lead', '34DRWDVB', 'active'),
-(3, 'Arya - PETRONAS', 'Arya@gmail.com', 'user@12345', NULL, NULL, 'pending');
+(3, 'Arya - PETRONAS', 'Arya@gmail.com', 'user@12345', NULL, NULL, 'pending'),
+(4, 'Thomas', 'Tom@gmail.com', 'User@12345', 'Reviewer', '34DRWDVB', 'active');
 
 --
 -- Indexes for dumped tables
@@ -77,6 +149,29 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `room_code`, `st
 ALTER TABLE `project`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `room_code` (`room_code`);
+
+--
+-- Indexes for table `proposal`
+--
+ALTER TABLE `proposal`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `project_id` (`project_id`),
+  ADD KEY `lead_id` (`lead_id`);
+
+--
+-- Indexes for table `proposal_attachments`
+--
+ALTER TABLE `proposal_attachments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `proposal_id` (`proposal_id`);
+
+--
+-- Indexes for table `proposal_reviews`
+--
+ALTER TABLE `proposal_reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `project_id` (`proposal_id`),
+  ADD KEY `reviewer_id` (`reviewer_id`);
 
 --
 -- Indexes for table `users`
@@ -93,13 +188,55 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `proposal`
+--
+ALTER TABLE `proposal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `proposal_attachments`
+--
+ALTER TABLE `proposal_attachments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `proposal_reviews`
+--
+ALTER TABLE `proposal_reviews`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `proposal`
+--
+ALTER TABLE `proposal`
+  ADD CONSTRAINT `proposal_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`),
+  ADD CONSTRAINT `proposal_ibfk_2` FOREIGN KEY (`lead_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `proposal_attachments`
+--
+ALTER TABLE `proposal_attachments`
+  ADD CONSTRAINT `proposal_attachments_ibfk_1` FOREIGN KEY (`proposal_id`) REFERENCES `proposal` (`id`);
+
+--
+-- Constraints for table `proposal_reviews`
+--
+ALTER TABLE `proposal_reviews`
+  ADD CONSTRAINT `proposal_reviews_ibfk_1` FOREIGN KEY (`proposal_id`) REFERENCES `project` (`id`),
+  ADD CONSTRAINT `proposal_reviews_ibfk_2` FOREIGN KEY (`reviewer_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
